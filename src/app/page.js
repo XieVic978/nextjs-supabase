385a9d6982947844314e8eaae118a3bfe9c4753a
Victor Xie <victor@ipick.ai>
2025-11-24 14:51:16 -0800

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
-import Image from "next/image";
+'use client';
+
+import useAuth from "@/hooks/useAuth";
+import {useRouter } from "next/navigation";
 
 export default function Home() {
+  const { user, loading } = useAuth();
+
+  if (!loading && user) {
+    router.push("/dashboard");
+    return null;
+  }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+      { loading ? <h1>Loading...</h1> : <h1>Login Form</h1> }
     </div>
   );
 }
--- src/components/context/AuthProvider.jsx
+++ src/components/context/AuthProvider.jsx
@@ -1,24 +1,27 @@
 'use client';
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 import client from "@/api/client";
-import { use } from "react/cjs/react.production";
 
 const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-}
 
   useEffect(() => {
-    client.auth.getSession().then(({ data }) => { setUser(data.session?.user || null); setLoading(false);
-});
-    const { data: listener } = client.auth.onAuthStateChange((event, session) => {
-      setUser(session?.user || null);
+    client.auth.getSession().then(({data}) => {
+      setUser(data?.session?.user || null);
+      setLoading(false);
+    }); 
+    const { data: listener } = client.auth.onAuthStateChange((e, session) => {
+      setUser(session.user || null);
     });
 
-    return () => { listener.subscription.unsubscribe(); };
+    return () => {
+      listener.subscription.unsubscribe();
+    }
+
   }, []);
 
   return (
@@ -26,3 +29,8 @@ const AuthProvider = ({ children }) => {
       {children}
     </AuthContext.Provider>
   );
+};
+
+export { AuthContext, AuthProvider };
+ 
+  
